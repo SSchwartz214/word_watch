d450463714f21677bf6d88785b5f3450b5ac6847
Seth Schwartz <sschwartz214@gmail.com>
2018-10-10 11:30:29 -0600

    completes gets and post

--- src/index.js
+++ src/index.js
@@ -1,5 +1,31 @@
 import $ from 'jquery'
 
-$(document).ready(() => {
-  // have fun!
-})
+const getTopWord = () => {
+  $('.word-count').html('')
+  fetch('https://wordwatch-api.herokuapp.com/api/v1/top_word')
+    .then((response) => response.json())
+    .then((rawWord) => appendTopWord(rawWord))
+    .catch((error) => console.error({ error }));
+}
+
+const appendTopWord = (data) => {
+  $('.word-count').append(`
+    ${Object.keys(data["word"])[0]}: ${Object.values(data["word"])[0]}
+  `)
+}
+
+const addNewWord = () => {
+  let word = $('#wordField').val();
+  fetch('https://wordwatch-api.herokuapp.com/api/v1/words', {
+    method: 'POST',
+    headers: { 'Content-Type': 'application/json' },
+    body: JSON.stringify({
+      word: { value: `${word}` }
+    })
+  })
+  getTopWord();
+}
+
+getTopWord();
+
+$('#break-btn').on('click', addNewWord);
